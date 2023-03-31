8e87806e12a4c53faa33ee47518a236c095eb49f
William Rogers <williamrogersdev@gmail.com>
2023-03-30 22:34:13 -0500

    practice_class

---
+++ hashtable.h
@@ -0,0 +1,34 @@
+
+
+#ifndef HASHTABLE_HASHTABLE_H
+#define HASHTABLE_HASHTABLE_H
+
+#include <iostream>
+#include "data.h"
+
+#define HASHTABLESIZE 15
+
+using std::cout;
+using std::endl;
+
+class Hashtable {
+
+public:
+    Hashtable();
+    ~Hashtable();
+    
+    bool insertEntry(int, string);
+    string getData(int);
+    bool deleteEntry(int id);
+    int getCount();
+    void printTable();
+
+    private:
+
+    int hash(int);
+    
+    Data *hashtable[HASHTABLESIZE];
+    int count;
+};
+
+#endif /* HASHTABLE_HASHTABLE_H */
---
+++ main.h
@@ -0,0 +1,20 @@
+
+#ifndef HASHTABLE_MAIN_H
+#define HASHTABLE_MAIN_H
+
+#include <iostream>
+#include <stdlib.h>    // srand, rand
+#include <string.h>    // strcpy
+#include <time.h>      // time
+
+#include "data.h"
+#include "hashtable.h"
+
+#define TESTDATASIZE 10
+#define MAXID 10000
+#define BUFFERSIZE 16
+
+using std::cout;
+using std::endl;
+
+#endif // HASHTABLE_MAIN_H
